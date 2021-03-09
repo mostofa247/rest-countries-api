0b3f43ee506e54fd2f06b6fd7d7bde1dafa09e7a
mostofa247 <74599998+mostofa247@users.noreply.github.com>
2021-03-09 06:45:54 -0500

    light mood

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
+
+
+
 const Header = () => {
+    const changeTheme = () => {
+        const moon = document.querySelector('.fa-moon')
+
+        moon.addEventListener('click', () => {
+            document.body.classList.toggle('light-theme')
+        })
+    }
+
     return (
         <div>
             <header className="header">
                 <h1>header</h1>
                 <div>
-                    <i className="fa fa-moon"></i>
+                    <i className="fa fa-moon" onClick={() => changeTheme()}></i>
                 </div>
             </header>
         </div>
